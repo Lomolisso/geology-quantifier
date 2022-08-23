6f86d4c322d3f81df9c6ac0ff314388d0194d3e3
EricContrerasP <43296737+EricContrerasP@users.noreply.github.com>
2022-08-23 13:24:06 -0400

    Algoritmos de prueba

---
+++ reducirResolucion.py
@@ -0,0 +1,16 @@
+import cv2
+
+#Porcentaje en el que se redimensiona la imagen
+scale_percent = 50
+src = cv2.imread('b3.jpg')
+#calcular el 50 por ciento de las dimensiones originales
+width = int(src.shape[1] * scale_percent / 100)
+height = int(src.shape[0] * scale_percent / 100)
+ 
+# dsize
+dsize = (width, height)
+ 
+# cambiar el tamaño de la image
+output = cv2.resize(src, dsize)
+ 
+cv2.imwrite('b4.png',output)
