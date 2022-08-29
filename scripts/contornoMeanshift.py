0dfa88bc5b91d1ca1887f748e1cb441ae63ca87f
Raúl De La Fuente <lomolisso.exe@gmail.com>
2022-08-29 18:31:32 -0400

    basic hist equalization + kmeans in color filter

--- scripts/contornoMeanshift.py
+++ scripts/contornoMeanshift.py
@@ -18,16 +18,70 @@ import numpy as np
 
 from skimage.filters import threshold_otsu
 
-img = cv2.imread('../img/b4.png')
+org_img = cv2.imread('../img/processed/isolated_long_jurel.png')
+
+cv2.imshow("original", org_img)
+
+def run_histogram_equalization(rgb_img):
+    # convert from RGB color-space to YCrCb
+    ycrcb_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2YCrCb)
+
+    # equalize the histogram of the Y channel
+    ycrcb_img[:, :, 0] = cv2.equalizeHist(ycrcb_img[:, :, 0])
+
+    # convert back to RGB color-space from YCrCb
+    return cv2.cvtColor(ycrcb_img, cv2.COLOR_YCrCb2BGR)
+
+
+img = run_histogram_equalization(org_img)
+cv2.imshow('equalized_img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
 img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 Z = np.float32(img.reshape((-1,3)))
 
-img = cv2.pyrMeanShiftFiltering(img, 20, 10, 3)
+#img = cv2.pyrMeanShiftFiltering(img, 50, 5, 3)
 img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
 
-plt.imshow(img)
-plt.show()
+#plt.imshow(img)
+#plt.show()
 
+
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+for K in range(2,10):
+    im = np.copy(img)
+    twoDimage = img.reshape((-1,3))
+    twoDimage = np.float32(twoDimage)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    attempts=10
+    ret,label,center=cv2.kmeans(twoDimage,K,None,criteria,attempts,cv2.KMEANS_PP_CENTERS)
+    center = np.uint8(center)
+    
+    res = center[label.flatten()]
+    result_image = res.reshape((img.shape))
+    #cv2.imshow('imagen',result_image)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    
+    label = label.flatten()
+    for cluster in range(0,K):
+        masked_image = np.copy(im)
+        # convert to the shape of a vector of pixel values
+        masked_image = masked_image.reshape((-1, 3))
+        # color (i.e cluster) to disable
+        
+        masked_image[label != cluster] = [0, 0, 0]
+
+        # convert back to original shape
+        masked_image = masked_image.reshape(im.shape)
+        # show the image
+        cv2.imshow(f'{cluster}',masked_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+"""
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(imgray, 127, 255, 0)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
@@ -37,10 +91,8 @@ cv2.imshow('imagen',img)
 cv2.imshow('thresh', thresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-"""
 img_rgb=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 img_gray=cv2.cvtColor(img_rgb,cv2.COLOR_RGB2GRAY)
-
 def filter_image(image, mask):
     r = image[:,:,0] * mask
     g = image[:,:,1] * mask
@@ -53,4 +105,3 @@ filtered = filter_image(img, img_otsu)
 cv2.imshow('imagen',filtered)
 cv2.waitKey(0)
 cv2.destroyAllWindows()"""
-
