32d44b16727a1e67ce0e197cbc3b13559f7f9832
Riul1999 <rodrigo.urrea@ing.uchile.cl>
2022-08-30 11:25:10 -0400

    added file management api

---
+++ scripts/api_fm.py
@@ -0,0 +1,31 @@
+"""
+Api for easy file management
+"""
+import tkinter, cv2
+from tkinter import filedialog
+
+# Create tk object and hide tk window.
+root = tkinter.Tk()
+root.withdraw()
+
+def load_image():
+    """
+    Method to load image from a selected path, using windows file system UI.
+    """
+    # Ask for the image to load.
+    filename = filedialog.askopenfilename(initialdir="../img", title="Select a File", filetypes=(("Image files","*.png *.jpg"), ("all files", ".*")))
+    root.withdraw()
+    # Return the readed image as a matrix.
+    return cv2.imread(filename)
+
+def save_image(img):
+    """
+    Method to save image from a selected path, using windows file system UI.
+    """
+    # Ask for the path where the image will be saved.
+    filename = filedialog.asksaveasfilename(initialdir="../img", title="Select a File", filetypes=(("Image files","*.png *.jpg"), ("all files", ".*")))
+    root.withdraw()
+    # Save image using cv2 save method.
+    # TODO get image type besides hardcode the type.
+    cv2.imwrite(f"{filename}.png", img)
+    return True
