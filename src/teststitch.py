bedf085d3dd2771f90f31cf618ff3e4284cd0b21
JorgeCVuwu <jorgecerda2008@gmail.com>
2022-10-03 18:29:56 -0300

    testing

--- src/teststitch.py
+++ src/teststitch.py
@@ -3,12 +3,17 @@ import sample_extraction
 import sample_extraction
 import cv2
 import image_managers
+import os
+
+c = 0
+path = './img/panoramic/samples_by_degree/diorite/'
 
 stitcher = stitching.Stitcher()
-# panorama = stitcher.stitch(["/img/raw/diorote/DIO 6N 0°.JPG", "/img/raw/diorote/DIO 6N 90°.JPG", "/img/raw/diorote/DIO 6N 180°.JPG"])
 raw_img1 = image_managers.load_image_from_window()
 # img = cv2.resize(raw_img1, (int(raw_img1.shape[1]*0.2),int(raw_img1.shape[0]*0.2)))
 img1 = sample_extraction.extract_sample(raw_img1)
+os.chdir(path)
+cv2.imwrite('image ' + str(c) + '.jpg', img1)
 
 raw_img2 = image_managers.load_image_from_window()
 # img = cv2.resize(raw_img1, (int(raw_img2.shape[1]*0.2),int(raw_img2.shape[0]*0.2)))
@@ -22,7 +27,19 @@ raw_img4 = image_managers.load_image_from_window()
 # img = cv2.resize(raw_img1, (int(raw_img4.shape[1]*0.2),int(raw_img4.shape[0]*0.2)))
 img4 = sample_extraction.extract_sample(raw_img4)
 
-panorama = stitcher.stitch([img1, img2, img3, img4])
+img1 = cv2.resize(img1, (200, 200))
+img2 = cv2.resize(img2, (200, 200))
+img3 = cv2.resize(img3, (200, 200))
+img4 = cv2.resize(img4, (200, 200))
 
+
+
+p_img1 = path + "dio_6n_0.jpg"
+p_img2 = path + 'dio_6n_90.jpg'
+p_img3 = path + 'dio_6n_180.jpg'
+p_img4 = path + 'dio_6n_270.jpg'
+
+panorama = stitcher.stitch([p_img1, p_img2])
 cv2.namedWindow('Sample Area')
 cv2.imshow('Sample Area', panorama)
+cv2.waitKey(0)
