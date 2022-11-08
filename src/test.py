29f9ebd73c6bb9b8a997d902994a7ffc5391f671
EricContrerasP <e.e.contreras.p@gmail.com>
2022-11-08 13:29:49 -0300

    area asserts

--- src/test.py
+++ src/test.py
@@ -23,6 +23,23 @@ def chessboardPattern(img,vertical,horizontal):
         for j in range(i%2,horizontal,2):
             cv2.rectangle(img, ( j*shape_y//horizontal,i*shape_x//vertical),((j+1)*shape_y//horizontal-1,(i+1)*shape_x//vertical-1), (255,255,255),-1)
 
+def math_area(array):
+    "calculo del area con determinante, los puntos deben estar ordenados y no crear agujeros en la figura"
+    detizq = 0
+    detder = 0
+    for i in range(len(array)):
+        if i ==len(array)-1:
+            detizq += array[i][0]*array[0][1]
+            detder += array[i][1]*array[0][0]
+        else:
+            detizq += array[i][0]*array[i+1][1]
+            detder += array[i][1]*array[i+1][0]
+    if 1/2*(detizq-detder)>0: 
+        return 1/2*(detizq-detder)
+    else: 
+        return -1/2*(detizq-detder)
+
+
 def test_area():
     height = 400
     width =400
@@ -57,48 +74,56 @@ def test_area():
     img = np.zeros((height,width,channels), dtype=np.uint8)
 
     #se testea circulos
-    cv2.circle(img, (10,10),10, (255,255,255), -1)
-    cv2.circle(img, (150,150),40, (255,0,255), -1)
-    cv2.circle(img, (300,50),50, (255,255,0), -1)
+    cv2.circle(img, (10,10),10, (255,255,255), -1,cv2.FILLED)
+    cv2.circle(img, (150,150),40, (255,0,255), -1,cv2.FILLED)
+    cv2.circle(img, (300,50),50, (255,255,0), -1,cv2.FILLED)
     data = contour_segmentation(img)
-    areas = 3.14*np.array([10,40,50])*np.array([10,40,50])
+    areas = np.pi*np.array([10*10,40*40,50*50])
     print(areas)
     for cnt in data:
-        print(cnt.get_area())
+        #TODO Assert with error.
+        cnt.get_area()
     cv2.imshow("blank_image", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     img = np.zeros((height,width,channels), dtype=np.uint8)
-    pts = np.array([[100,100],[100,200],[200,200]], np.int32)
+    #triangulos de alto 100 y base 100
+    pts = np.array([[100,100],[100,200],[200,200]])
+    print(math_area(pts))
     pts = pts.reshape((-1,1,2))
     img = cv2.fillPoly(img,[pts],(0,255,255))
-    pts = np.array([[250,250],[350,250],[300,350]], np.int32)
+    pts = np.array([[250,250],[350,250],[300,350]])
+    print(math_area(pts))
     pts = pts.reshape((-1,1,2))
     img = cv2.fillPoly(img,[pts],(0,255,255))
-    pts = np.array([[0,200],[0,300],[100,260]], np.int32)
+    pts = np.array([[0,200],[0,300],[100,260]])
+    print(math_area(pts))
     pts = pts.reshape((-1,1,2))
     img = cv2.fillPoly(img,[pts],(0,255,255))
-    for cnt in data:
-        print(cnt.aspect_ratio() == 1)
     data = contour_segmentation(img)
+    for cnt in data:
+        assert(cnt.get_area()==5000)
     cv2.imshow("blank_image", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
     img = np.zeros((height,width,channels), dtype=np.uint8)
     pts = np.array([[100,100],[100,200],[200,200],[150,150],[200,100]], np.int32)
+    print(math_area(pts))
     pts = pts.reshape((-1,1,2))
     img = cv2.fillPoly(img,[pts],(0,255,255))
     pts = np.array([[250,250], [350,250],[280,200],[300,300],[320,200]], np.int32)
+    print(math_area(pts))
     pts = pts.reshape((-1,1,2))
     img = cv2.fillPoly(img,[pts],(0,255,255))
-    pts = np.array([[0,200],[0,300],[100,260], [60,220]], np.int32)
+    pts = np.array([[0,200],[0,300],[100,260], [50,200]], np.int32)
+    print(math_area(pts))
     pts = pts.reshape((-1,1,2))
     img = cv2.fillPoly(img,[pts],(0,255,255))
-    for cnt in data:
-        print(cnt.aspect_ratio() == 1)
     data = contour_segmentation(img)
+    for cnt in data:
+        print(cnt.get_area())
     cv2.imshow("blank_image", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -115,26 +140,24 @@ def test_aspect_ratio():
     cv2.rectangle(img, (150,150),(200,200), (255,0,255), -1)
     cv2.rectangle(img, (200,0),(210,10), (255,255,0), -1)
     data = contour_segmentation(img)
+    #los cuadrados tienen ratio 1 siempre
     for cnt in data:
-        print(cnt.aspect_ratio() == 1)
-    cv2.imshow("blank_image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        assert(cnt.aspect_ratio() == 1)
+
+    #se limpia la imagen.
     img = np.zeros((height,width,channels), dtype=np.uint8)
     #se testea rectangulos
     cv2.rectangle(img, (0,0),(140,100), (255,255,255), -1)
-    #1.4
     cv2.rectangle(img, (150,150),(249,199), (255,0,255), -1)
-    #2 rectangle considera el pixel final como perteneciente a rectangulo a dibujar.
+    #rectangle considera el pixel final e inicial como perteneciente a rectangulo a dibujar, por lo que su aspect ratio es mayor.
     cv2.rectangle(img, (200,0),(219,9), (255,255,0), -1)
-    #2
     data = contour_segmentation(img)
+    aspect_ratios = [1.4, 2]
     for cnt in data:
-        #TODO asserts
         print(cnt.aspect_ratio())
-    cv2.imshow("blank_image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        assert(cnt.aspect_ratio() in aspect_ratios)
+    
+    #se limpia la imagen.
     img = np.zeros((height,width,channels), dtype=np.uint8)
 
     #se testea circulos
@@ -142,12 +165,11 @@ def test_aspect_ratio():
     cv2.circle(img, (150,150),40, (255,0,255), -1)
     cv2.circle(img, (300,50),50, (255,255,0), -1)
     data = contour_segmentation(img)
+    #todos los circulos son de ratio 1
     for cnt in data:
-        print(cnt.aspect_ratio() == 1)
-    cv2.imshow("blank_image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        assert(cnt.aspect_ratio() == 1)
 
+    #se limpia la imagen
     img = np.zeros((height,width,channels), dtype=np.uint8)
     pts = np.array([[100,100],[100,200],[200,200]], np.int32)
     pts = pts.reshape((-1,1,2))
@@ -158,13 +180,12 @@ def test_aspect_ratio():
     pts = np.array([[0,200],[0,300],[100,260]], np.int32)
     pts = pts.reshape((-1,1,2))
     img = cv2.fillPoly(img,[pts],(0,255,255))
+    #los 3 triangulos tienen aspect ratio 1
     for cnt in data:
-        print(cnt.aspect_ratio() == 1)
+        assert(cnt.aspect_ratio() == 1)
     data = contour_segmentation(img)
-    cv2.imshow("blank_image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
     
+    #se limpia la imagen
     img = np.zeros((height,width,channels), dtype=np.uint8)
     pts = np.array([[100,100],[100,200],[200,200],[150,150],[200,100]], np.int32)
     pts = pts.reshape((-1,1,2))
@@ -175,16 +196,12 @@ def test_aspect_ratio():
     pts = np.array([[0,200],[0,300],[100,260], [60,220]], np.int32)
     pts = pts.reshape((-1,1,2))
     img = cv2.fillPoly(img,[pts],(0,255,255))
+    #poligonos de ratio 1
     for cnt in data:
-        print(cnt.aspect_ratio() == 1)
-    data = contour_segmentation(img)
-    cv2.imshow("blank_image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
+        assert(cnt.aspect_ratio() == 1)
 
 test_area()
-test_aspect_ratio()
+#test_aspect_ratio()
 
 
 """
