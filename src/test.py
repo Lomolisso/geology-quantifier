19a4a3c8e4ada13d4ba34d5213fe3890b3102288
EricContrerasP <e.e.contreras.p@gmail.com>
2022-11-22 11:33:13 -0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import percent as pc
 import random
-from shape_detection import contour_segmentation
+from shape_detection import contour_segmentation, ContourData
 
 def img_with_percent(img, percent):
     shape_x,shape_y = img.shape[0], img.shape[1]
@@ -60,6 +60,71 @@ def poli_gen(n, center, radius):
     points = [[round(x+radius*np.cos(a),0),round(y+radius*np.sin(a),0)] for a in theta]
     return np.array(points, np.int32)
 
+def poli_gen_radio(n, center, radio_random):
+    x, y = center
+    theta = np.sort(np.random.rand(n))
+    theta = 2*np.pi*theta
+    radio = (radio_random+20)-radio_random*np.random.rand(n)
+    points = [[round(x+radio[i]*np.cos(theta[i]),0),round(y+radio[i]*np.sin(theta[i]),0)] for i in range(len(theta))]
+    return np.array(points, np.int32)
+
+def assert_error(estimate_value, calculated_value, error):
+    error_calculado = (calculated_value-estimate_value)/estimate_value
+    print(error_calculado)
+    assert(error>abs(error_calculado))
+
+def testing(funcion_programa, funcion_math):
+    height = 400
+    width =400
+    channels = 3
+    lados = [10, 50, 100]
+    
+    points = []
+    points.append(np.array([[100,100],[100,200],[200,200]]))
+    points.append(np.array([[250,250],[350,250],[300,350]]))
+    points.append(np.array([[0,200],[0,300],[100,260]]))
+    points.append(np.array([[100,100],[100,200],[200,200],[150,150],[200,100]], np.int32))  
+    points.append(np.array([[250,250],[300,300], [350,250],[320,200],[280,200]], np.int32))
+    points.append(np.array([[0,200],[0,300],[100,260], [70,200]], np.int32))
+    
+    for pts in points:
+        img = np.zeros((height,width,channels), dtype=np.uint8)
+        value = funcion_math(pts)
+        pts = pts.reshape((-1,1,2))
+        img = cv2.fillPoly(img,[pts],(0,255,255))
+        data = contour_segmentation(img)
+        #revisar diagonales dificiles
+        assert_error(value, funcion_programa(data[0]), 0.02)
+    
+    for i in range(3,100):
+        img = np.zeros((height,width,channels), dtype=np.uint8)
+        pts = poli_gen(i,(200,200),100)
+        value = funcion_math(pts)
+        pts = pts.reshape((-1,1,2))
+        img = cv2.fillPoly(img,[pts],(0,255,255))
+        data = contour_segmentation(img)
+        assert_error(value, funcion_programa(data[0]), 0.02)
+    while(True):
+        for i in range(10,100):
+            img = np.zeros((height,width,channels), dtype=np.uint8)
+            pts = poli_gen_radio(i,(200,200),100)
+            value = funcion_math(pts)
+            pts = pts.reshape((-1,1,2))
+            img = cv2.fillPoly(img,[pts],(0,255,255))
+            
+            data = contour_segmentation(img)
+            value2 = [i[0] for i in data[0].contour]
+            a = funcion_math(value2)
+            try:
+                assert_error(value, funcion_programa(data[0]), 0.1)
+            except:
+                cv2.imshow("a",img)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+        
+
+testing(ContourData.get_area, math_area)
+
 def test_area():
     height = 400
     width =400
@@ -72,10 +137,8 @@ def test_area():
     areas = [100,2500,10000]
     data = contour_segmentation(img)
     for cnt in data:
-        print((cnt.get_area() in areas))    
-    cv2.imshow("blank_image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        assert((cnt.get_area() in areas))
+
     img = np.zeros((height,width,channels), dtype=np.uint8)
     #se testea rectangulos
     cv2.rectangle(img, (0,0),(140,100), (255,255,255), -1)
@@ -87,87 +150,31 @@ def test_area():
     areas = [14000,5000,200]
     data = contour_segmentation(img)
     for cnt in data:
-        print((cnt.get_area() in areas)) 
-    cv2.imshow("blank_image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    img = np.zeros((height,width,channels), dtype=np.uint8)
+        assert((cnt.get_area() in areas)) 
+    
 
     #se testea circulos
-    cv2.circle(img, (10,10),10, (255,255,255), -1,cv2.FILLED)
-    cv2.circle(img, (150,150),40, (255,0,255), -1,cv2.FILLED)
-    cv2.circle(img, (300,50),50, (255,255,0), -1,cv2.FILLED)
-    data = contour_segmentation(img)
     areas = np.pi*np.array([10*10,40*40,50*50])
-    print(areas)
-    for cnt in data:
-        #TODO Assert with error.
-        print(cnt.get_area())
-    cv2.imshow("blank_image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    radio = range(10, 150, 10)
+    for r in radio:
+        img = np.zeros((height,width,channels), dtype=np.uint8)
+        cv2.circle(img, (200,200),r, (255,255,255), -1,cv2.FILLED)
+        data = contour_segmentation(img)
+        area_math = np.pi*r*r
+        assert_error(area_math,data[0].get_area(), 0.02)
 
-    img = np.zeros((height,width,channels), dtype=np.uint8)
-    #triangulos de alto 100 y base 100
-    pts = np.array([[100,100],[100,200],[200,200]])
-    print(math_area(pts))
-    pts = pts.reshape((-1,1,2))
-    img = cv2.fillPoly(img,[pts],(0,255,255))
-    pts = np.array([[250,250],[350,250],[300,350]])
-    print(math_area(pts))
-    pts = pts.reshape((-1,1,2))
-    img = cv2.fillPoly(img,[pts],(0,255,255))
-    pts = np.array([[0,200],[0,300],[100,260]])
-    print(math_area(pts))
-    pts = pts.reshape((-1,1,2))
-    img = cv2.fillPoly(img,[pts],(0,255,255))
-    data = contour_segmentation(img)
-    for cnt in data:
-        assert(cnt.get_area()==5000)
-    cv2.imshow("blank_image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
-    areas = []
-    img = np.zeros((height,width,channels), dtype=np.uint8)
-
-    pts = np.array([[100,100],[100,200],[200,200],[150,150],[200,100]], np.int32)
-    areas.append(math_area(pts))
-    pts = pts.reshape((-1,1,2))
-    img = cv2.fillPoly(img,[pts],(0,255,255))
-    pts = np.array([[250,250],[300,300], [350,250],[320,200],[280,200]], np.int32)
-    areas.append(math_area(pts))
-    pts = pts.reshape((-1,1,2))
-    img = cv2.fillPoly(img,[pts],(0,255,255))
-    pts = np.array([[0,200],[0,300],[100,260], [60,200]], np.int32)
-    areas.append(math_area(pts))
-    pts = pts.reshape((-1,1,2))
-    img = cv2.fillPoly(img,[pts],(0,255,255))
-    data = contour_segmentation(img)
-    
-    for cnt in data:
-        #revisar diagonales dificiles
-        assert(cnt.get_area() in areas)
-    cv2.imshow("blank_image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
     archivo = open("test_area.txt", 'w')
+    archivo.write("lados,area_matematica,area_programa,error\n")
     for i in range(3,100):
         img = np.zeros((height,width,channels), dtype=np.uint8)
         pts = poli_gen(i,(200,200),100)
         area_math = math_area(pts)
         pts = pts.reshape((-1,1,2))
         img = cv2.fillPoly(img,[pts],(0,255,255))
-        cv2.imshow("blank_image", img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
         data = contour_segmentation(img)
-        print(f"poligono de {i} lados")
-        print(f"area calculada de forma matematica: {area_math}")
-        print(f"area calculada por el programa: {data[0].get_area()}")
-        print(f"error: {(data[0].get_area()-area_math)/area_math}")
-        archivo.write(f"poligono de {i} lados\narea calculada de forma matematica: {area_math}\narea calculada por el programa: {data[0].get_area()}\nerror: {(data[0].get_area()-area_math)/area_math}\n")
+        archivo.write(f"{i},{area_math},{data[0].get_area()},{(data[0].get_area()-area_math)/area_math}\n")
+        
     archivo.close()
 
 def test_aspect_ratio():
@@ -241,6 +248,7 @@ def test_aspect_ratio():
         assert(cnt.aspect_ratio() == 1)
 
     archivo = open("test_ratio.txt", 'w')
+    archivo.write("lados,ratio_matematico,ratio_programa,error\n")
     for i in range(3,100):
         img = np.zeros((height,width,channels), dtype=np.uint8)
         pts = poli_gen(3,(200,200),100)
@@ -249,16 +257,10 @@ def test_aspect_ratio():
         pts = pts.reshape((-1,1,2))
         img = cv2.fillPoly(img,[pts],(0,255,255))
         data = contour_segmentation(img)
-        print(f"poligono de {i} lados")
-        print(f"ratio calculado de forma matematica: {ratio_math}")
-        print(f"ratio calculado por el programa: {data[0].aspect_ratio()}")
-        print(f"error: {(data[0].aspect_ratio()-ratio_math)/ratio_math}")
-        archivo.write(f"poligono de {i} lados\nratio calculado de forma matematica: {ratio_math}\nratio calculado por el programa: {data[0].aspect_ratio()}\nerror: {(data[0].aspect_ratio()-ratio_math)/ratio_math}\n")
+        archivo.write(f"{i},{ratio_math},{data[0].aspect_ratio()},{(data[0].aspect_ratio()-ratio_math)/ratio_math}\n")
     archivo.close()
 
 #test_area()
-test_aspect_ratio()
+#test_aspect_ratio()
 
-#pts = np.array([[100,100],[100,200],[200,200],[150,150],[200,100]], np.int32)
-#print(math_aspect_ratio(pts))
 
