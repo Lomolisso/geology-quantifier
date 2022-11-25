ea865f67d4ad4f40eea09b9cefb928b602511314
Magdalena-Alvarez <magdalena.alvarez@ing.uchile.cl>
2022-11-25 14:50:23 -0300

    create unit button

--- src/gui.py
+++ src/gui.py
@@ -23,6 +23,7 @@ SCREEN_HEIGHT = ROOT.winfo_screenheight()
 ARROW_LEFT = tkinter.PhotoImage(file=get_path("./assets/left_arrow.png"))
 ARROW_RIGHT = tkinter.PhotoImage(file=get_path("./assets/right_arrow.png"))
 HELP_ICON = tkinter.PhotoImage(file=get_path("./assets/help_icon.png"))
+ON = tkinter.PhotoImage(file=get_path("./assets/on_off2.png"))
 
 class GUI(object):
     """
@@ -188,6 +189,10 @@ class GUI(object):
         btn_down_name = 'Bajar'
         btn_down_description = 'Permite cambiar la imagen actual por la imagen seleccionada.'
         self.btn_down, self.hover_down = createButtonWithHover(self.navigate_fr, btn_down_name, self.down, btn_down_description,image=ARROW_RIGHT)
+        
+        btn_switch_unit_name = 'CM'
+        btn_switch_unit_description = 'Permite cambiar la unidad de medida entre centímetro o milímetros.'
+        self.btn_switch_unit, self.hover_units = createButtonWithHover(self.results_fr, btn_switch_unit_name, self.switch_unit, btn_switch_unit_description,image=ON)
 
         btn_doc_name = 'Ayuda'
         btn_doc_description = 'Permite abrir la documentación completa de la aplicación.'
@@ -221,9 +226,12 @@ class GUI(object):
         self.grados = 0
         self.canvas_preview = tkinter.Canvas(self.principal_fr)
         self.prev_boolean = False
+        self.cm = False
    
     def set_height(self):
         try:
+            if not self.cm:
+                self.height_cm = self.height_cm*0.1
             self.height_cm = float(self.entry_height_cm.get())
         except:
             tkinter.messagebox.showwarning("Error", message="Por favor ingresa un número.")
@@ -898,9 +906,10 @@ class GUI(object):
             for col_num in range(len(sc.STATISTICS)):
                 self.create_label(aggregated_results[row_num][col_num], row_num+1, col_num+2)
             self.create_label(percent.percent(images[row_num]) ,row_num+1, len(sc.STATISTICS)+2)
-
+        
         self.btnExport = ttk.Button(self.results_fr, text="Descargar", width=15, command=lambda : self.table_to_csv(results, len(colors)) , cursor='arrow')
         self.btnExport.grid(row=len(aggregated_results) + 1, column=len(sc.STATISTICS) // 2 + 1)
+        self.btn_switch_unit.grid(row=len(aggregated_results) + 1, column=len(sc.STATISTICS) // 2 + 2)
     
     def table_to_csv(self, results, cluster_num) -> None:
         """
@@ -977,6 +986,9 @@ class GUI(object):
         self.sample_extractor.to_unwrapping()
         self._set_extractor_canvas()
 
+    def switch_unit(self) -> None:
+
+        self.cm = not self.cm
 
 
 
