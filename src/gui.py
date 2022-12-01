4959388c72e03a7f74ee4989380af53498621da5
Riul1999 <rodrigo.urrea@ing.uchile.cl>
2022-12-01 10:41:53 -0400

    refactor creation and cleaning of the buttons related to sample extraction

--- src/gui.py
+++ src/gui.py
@@ -603,19 +603,7 @@ class GUI(object):
         self.prev_boolean = False
         self.org_img = self.choose_cut_method(self.rot_img)
         self.main_win.unbind("<Key>")
-        self.un_measures()
-        for wget in self.file_fr.winfo_children():
-            wget.grid_forget()
-        self.file_fr.grid_forget()
-        for wget in self.command_fr.winfo_children():
-            wget.grid_forget()
-        self.command_fr.grid_forget()
-        for wget in self.crop_fr.winfo_children():
-            wget.grid_forget()
-        self.crop_fr.grid_forget()
-        for wget in self.size_fr.winfo_children():
-            wget.grid_forget()
-        self.size_fr.grid_forget()
+        self.clean_se_btns()
         self.show_img()
 
     def reset_image(self):
@@ -676,25 +664,57 @@ class GUI(object):
         # insert in  canvas
         self._set_extractor_canvas()
 
-    def measures(self):
+    def create_se_btns(self):
         """
-        Sets frame of measures
+        Method to grid all the buttons related to the sample extraction of the image.
+        It also grid the frames of every button.
         """
+        
+        # -- File managment
+        self.file_fr.grid(row=0, column=0, sticky=tkinter.N)
+        self.btn_img.grid(row=0, column=0, padx=5, pady=5)
+        self.file_fr_lbl.grid(column=0)
+        # -- commands --
+        self.command_fr.grid(row=0, column=1, sticky=tkinter.N)
+        self.btn_save_img.grid(row=0, column=0, padx=5, pady=5)
+        self.btn_reset_img.grid(row=1, column=0, padx=5, pady=5)
+        self.btn_select_all_img.grid(row=1, column=1, padx=5, pady=5)
+        self.btn_rotate.grid(row=0, column=1, padx=5, pady=5)
+        self.btn_rotateR.grid(row=0, column=2, padx=5, pady=5)
+        self.btn_rotateL.grid(row=1, column=2, padx=5, pady=5)
+        self.command_fr_lbl.grid(column=0, padx=5, pady=5, columnspan=3)
+        # -- crop types --
+        self.crop_fr.grid(row=0, column=2, sticky=tkinter.N)
+        self.btn_rectangle.grid(row=0, column=0, padx=5, pady=5)
+        self.btn_unwrapping.grid(row=1, column=0, padx=5, pady=5)
+        self.btn_free.grid(row=2, column=0, padx=5, pady=5)
+        self.crop_fr_lbl.grid(column=0, padx=5, pady=5)
+        # -- size tools --
         self.size_fr.grid(row=0, column=4, sticky=tkinter.N)
         self.size_sub_fr.grid(row=0, column=0)
         self.entry_height_cm.grid(row=0, column=0)
         self.btn_height.grid(row=0, column=1)
         self.size_fr_lbl.grid(row=1, column=0)
+        # -- help --
+        self.help_fr.grid(row=0, column=5, sticky=tkinter.N)
+        self.btn_doc.grid(row=0, column=0, padx=5, pady=5)
 
-    def un_measures(self):
+    def clean_se_btns(self) -> None:
         """
-        Clean frame of measures
+        Method to destroy every frame and its widgets related to the sample extraction, is used to clean the screen to display new information.
         """
-        self.entry_height_cm.grid_forget()
-        self.btn_height.grid_forget()
-        self.size_fr_lbl.grid_forget()
+        for wget in self.file_fr.winfo_children():
+            wget.grid_forget()
+        self.file_fr.grid_forget()
+        for wget in self.command_fr.winfo_children():
+            wget.grid_forget()
+        self.command_fr.grid_forget()
+        for wget in self.crop_fr.winfo_children():
+            wget.grid_forget()
+        self.crop_fr.grid_forget()
+        for wget in self.size_fr.winfo_children():
+            wget.grid_forget()
         self.size_fr.grid_forget()
-        self.size_sub_fr.grid_forget()
 
     def select_img(self):
         """
@@ -740,30 +760,7 @@ class GUI(object):
             self.sample_extractor.set_image(self._resize_img(resize_img))
             self.crop()
 
-            self.measures()
-
-            # -- File managment
-            self.file_fr.grid(row=0, column=0, sticky=tkinter.N)
-            self.btn_img.grid(row=0, column=0, padx=5, pady=5)
-            self.file_fr_lbl.grid(column=0)
-            # -- commands --
-            self.command_fr.grid(row=0, column=1, sticky=tkinter.N)
-            self.btn_save_img.grid(row=0, column=0, padx=5, pady=5)
-            self.btn_reset_img.grid(row=1, column=0, padx=5, pady=5)
-            self.btn_select_all_img.grid(row=1, column=1, padx=5, pady=5)
-            self.btn_rotate.grid(row=0, column=1, padx=5, pady=5)
-            self.btn_rotateR.grid(row=0, column=2, padx=5, pady=5)
-            self.btn_rotateL.grid(row=1, column=2, padx=5, pady=5)
-            self.command_fr_lbl.grid(column=0, padx=5, pady=5, columnspan=3)
-            # -- crop types --
-            self.crop_fr.grid(row=0, column=2, sticky=tkinter.N)
-            self.btn_rectangle.grid(row=0, column=0, padx=5, pady=5)
-            self.btn_unwrapping.grid(row=1, column=0, padx=5, pady=5)
-            self.btn_free.grid(row=2, column=0, padx=5, pady=5)
-            self.crop_fr_lbl.grid(column=0, padx=5, pady=5)
-            # -- help --
-            self.help_fr.grid(row=0, column=5, sticky=tkinter.N)
-            self.btn_doc.grid(row=0, column=0, padx=5, pady=5)
+            self.create_se_btns()
 
             self.segmentation = False
             self.grados = 0
